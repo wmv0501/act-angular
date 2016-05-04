5248476cbc160e2525a80200d00dd858b7404504
wvergara <wvergara@owens.com>
2016-05-04 04:57:34 -0400

    home page finished

--- angular/controller/homeController.js
+++ angular/controller/homeController.js
@@ -1,7 +1,12 @@
 angular
     .module('mainApp')
-    .controller('homeCtrl', function($scope, $http){
+    .controller('homeCtrl', function($scope, $http, $sce){
         $http.get('/angular/data/home.json').success (function(data2){
         $scope.data = data2;
         });
-    });
+
+
+    }).filter('trustAsResourceUrl', ['$sce', function($sce) {
+           return function(val) {
+               return $sce.trustAsResourceUrl(val);
+           }}]);
